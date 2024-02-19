54b7eed031c2852d7125894317995db3cb5d162c
nico877h <Nico877h@elev.tec.dk>
2024-02-19 17:00:32 +0100

    justhealth

---
+++ My project (1)/Assets/health.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+    
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public healthbar healthbar;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthbar.SetMaxHealth(maxHealth);
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            TakeDamage(20);
+        }
+    
+    
+    void TakeDamage(int damage)
+        {
+            currentHealth -= damage;
+
+            healthbar.SetHealth(currentHealth);
+        }
+    }
+
+}
